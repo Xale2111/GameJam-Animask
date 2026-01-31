2a97bea8cf67f9bca6e1986d61f9d905da473fd1
mebearwhodis <144151605+mebearwhodis@users.noreply.github.com>
2026-01-31 11:33:21 +0100

    Completed: DarknessManager

--- Assets/Scripts/DarknessManager.cs
+++ Assets/Scripts/DarknessManager.cs
@@ -1,17 +1,18 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering.Universal;
 
-public class DarknessAbilityController : MonoBehaviour
+public class DarknessController : MonoBehaviour
 {
     [Header("Lights")]
     [SerializeField] private Light2D globalLight;   // Global Light 2D
     [SerializeField] private Light2D playerLight;   // Point Light 2D on player
-
-    private const float NormalGlobalIntensity = 1.0f;
-    private const float DarkGlobalIntensity = 0.0f;
-    private const float LitCircleIntensity = 1.0f;
+    [SerializeField] private float normalGlobalIntensity = 1.0f;
+    [SerializeField] private float darkGlobalIntensity = 0.0f;
+    [SerializeField] private float normalPlayerIntensity = 0.0f;
+    [SerializeField] private float darkPlayerIntensity = 1.0f;
 
     [Header("Timings")]
     [SerializeField] private float fadeToBlackTime = 0.25f;
@@ -19,32 +20,21 @@ public class DarknessAbilityController : MonoBehaviour
     [SerializeField] private float fadeCircleOutTime = 0.15f;
     [SerializeField] private float fadeBackToNormalTime = 0.25f;
     [SerializeField] private float circleDelayAfterBlack = 0.10f;
-    [SerializeField] private float globalDelayAfterCircleOff = 0.05f;
+    [SerializeField] private float globalDelayAfterCircleOff = 0.10f;
 
+    private bool _eyesClosed;
     private Coroutine _sequence;
 
-    // void OnCloseEyes()
-    // {
-    //     holdAction.action.Enable();
-    //     holdAction.action.started += OnHoldStarted;
-    //     holdAction.action.canceled += OnHoldCanceled;
-    // }
-    //
-    // void OnOpenEyes()
-    // {
-    //     holdAction.action.started -= OnHoldStarted;
-    //     holdAction.action.canceled -= OnHoldCanceled;
-    //     holdAction.action.Disable();
-    // }
-
-    private void OnHoldStarted(InputAction.CallbackContext _)
+    public void OnCloseEyes(InputAction.CallbackContext context)
     {
-        StartSequence(HoldRoutine());
-    }
+        if (context.performed)
+        {
+            return;
+        }
 
-    private void OnHoldCanceled(InputAction.CallbackContext _)
-    {
-        StartSequence(ReleaseRoutine());
+        _eyesClosed = !_eyesClosed;
+        
+        StartSequence(_eyesClosed ? CloseEyesRoutine() : OpenEyesRoutine());
     }
 
     private void StartSequence(IEnumerator routine)
@@ -53,49 +43,47 @@ public class DarknessAbilityController : MonoBehaviour
         _sequence = StartCoroutine(routine);
     }
 
-    private IEnumerator HoldRoutine()
+    private IEnumerator CloseEyesRoutine()
     {
-        // playerLight.intensity = 0f;
-
         // Fade whole screne to black
-        yield return FadeLight(globalLight, globalLight.intensity, DarkGlobalIntensity, fadeToBlackTime);
+        yield return FadeLight(globalLight, globalLight.intensity, darkGlobalIntensity, fadeToBlackTime);
 
         // Light Circle
         if (circleDelayAfterBlack > 0f)
             yield return new WaitForSeconds(circleDelayAfterBlack);
 
-        yield return FadeLight(playerLight, playerLight.intensity, LitCircleIntensity, fadeCircleInTime);
+        yield return FadeLight(playerLight, playerLight.intensity, darkPlayerIntensity, fadeCircleInTime);
     }
 
-    private IEnumerator ReleaseRoutine()
+    private IEnumerator OpenEyesRoutine()
     {
         // Remove Circle
-        yield return FadeLight(playerLight, playerLight.intensity, 0f, fadeCircleOutTime);
+        yield return FadeLight(playerLight, playerLight.intensity, normalPlayerIntensity, fadeCircleOutTime);
 
         // Then Restore GlobalLight
         if (globalDelayAfterCircleOff > 0f)
             yield return new WaitForSeconds(globalDelayAfterCircleOff);
 
-        yield return FadeLight(globalLight, globalLight.intensity, NormalGlobalIntensity, fadeBackToNormalTime);
+        yield return FadeLight(globalLight, globalLight.intensity, normalGlobalIntensity, fadeBackToNormalTime);
     }
 
-    private static IEnumerator FadeLight(Light2D light, float from, float to, float duration)
+    private static IEnumerator FadeLight(Light2D light, float currentIntensity, float intensityGoal, float duration)
     {
         if (!light) yield break;
 
         if (duration <= 0f)
         {
-            light.intensity = to;
+            light.intensity = intensityGoal;
             yield break;
         }
 
-        float t = 0f;
-        while (t < duration)
+        float timer = 0f;
+        while (timer < duration)
         {
-            t += Time.deltaTime;
-            light.intensity = Mathf.Lerp(from, to, t / duration);
+            timer += Time.deltaTime;
+            light.intensity = Mathf.Lerp(currentIntensity, intensityGoal, timer / duration);
             yield return null;
         }
-        light.intensity = to;
+        light.intensity = intensityGoal;
     }
 }
